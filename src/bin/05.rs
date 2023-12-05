2a13c6f5297eb4fe1bb31aec887689000a107516
fgdou <fabigoardou@gmail.com>
2023-12-05 15:00:50 +0100

    d05p1

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,52 +1,90 @@
-type num = u32;
+type Num = u64;
 
-type Seeds = Vec<num>;
+type Seeds = Vec<Num>;
 
-struct Range {
-    pub from: num,
-    pub to: num,
-    pub size: num
+trait NextRange {
+    fn next(&self, n: Num) -> Option<Num>;
 }
 
-type map = Vec<Range>;
+#[derive(Debug)]
+struct Range {
+    pub from: Num,
+    pub to: Num,
+    pub size: Num
+}
+
+type RangeMap = Vec<Range>;
+
+#[derive(Debug)]
 struct Input {
     pub seeds: Seeds,
-    pub maps: Vec<map>,
+    pub maps: Vec<RangeMap>,
+}
+
+impl NextRange for Range {
+    fn next(&self, n: Num) -> Option<Num> {
+        if n < self.from {
+            return None;
+        }
+        let diff = n - self.from;
+        if diff < self.size {
+            Some(self.to + (n-self.from))
+        } else {
+            None
+        }
+    }
+}
+impl NextRange for Vec<Range> {
+    fn next(&self, n: Num) -> Option<Num> {
+        self.iter().find_map(|e| e.next(n))
+    }
 }
 
 fn parse_seeds(input: &str) -> Seeds {
     let input = &input[7..];
-    input.split(" ").map(|s| s.parse::<num>().unwrap()).collect()
+    input.split(" ").map(|s| s.parse::<Num>().unwrap()).collect()
 }
 
-fn parse_map(input: &str) -> map {
-    let lines = input.lines();
+fn parse_map(input: &str) -> RangeMap {
+    let mut lines = input.lines();
+    lines.next().unwrap();
 
     lines.map(|l| {
-        let nums = l.split(" ").map(|n| n.parse::<num>().unwrap()).collect::<Vec<num>>();
+        let nums = l.split(" ").map(|n| n.parse::<Num>().unwrap()).collect::<Vec<Num>>();
         assert!(nums.len() == 3);
         Range {
             from: nums[1],
             to: nums[0],
             size: nums[2]
         }
-    }).collect::<map>()
+    }).collect::<RangeMap>()
 }
 
 fn parse(input: &str) -> Input {
     let mut lines = input.split("\n\n");
     let seeds = parse_seeds(lines.next().unwrap());
-    lines.next().unwrap();
 
     let maps = lines.map(|l| parse_map(l)).collect();
     Input{seeds, maps}
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<Num> {
+    let input = parse(input);
+
+    dbg!(&input);
+
+    let seeds_end = input.seeds.iter()
+        .map(|seed| {
+            input.maps.iter()
+                .fold(*seed, |n, map| map.next(n).unwrap_or(n))  
+        });
+
+    Some(
+        seeds_end.min().unwrap()
+    )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<Num> {
     None
 }
 
@@ -68,3 +106,7 @@ mod tests {
         assert_eq!(result, None);
     }
 }
+
+
+// 286952859
+// 278755257
