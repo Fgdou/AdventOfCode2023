daaba27414b6b783c5259ca02ee222809c3e3652
fgdou <fabigoardou@gmail.com>
2023-12-06 09:06:02 +0100

    06p2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -9,9 +9,9 @@ use std::ops::Range;
 /// => t_button*t_max - t_button^2 - target > 0
 /// 
 /// solution => (t_max - sqrt(t_max^2 - 4*target))/2 < b < (t_max + sqrt(t_max^2 - 4*target))/2
-fn solve_equation(t_max: u32, target: u32) -> Range<u32> {
-    let t_max = t_max as f32;
-    let n = target as f32;
+fn solve_equation(t_max: u64, target: u64) -> Range<u64> {
+    let t_max = t_max as f64;
+    let n = target as f64;
 
     let delta = t_max.powi(2)-4.0*n;
     let detla_sqrt = delta.sqrt();
@@ -19,39 +19,52 @@ fn solve_equation(t_max: u32, target: u32) -> Range<u32> {
     let left = (t_max - detla_sqrt)/2.0+0.01;
     let right = (t_max + detla_sqrt)/2.0-0.01;
 
-    left.ceil().max(0.0) as u32..right.ceil().min(t_max) as u32
+    left.ceil().max(0.0) as u64..right.ceil().min(t_max) as u64
 }
 
 #[derive(Debug)]
 struct Pair {
-    time: u32,
-    distance: u32
+    time: u64,
+    distance: u64
 }
 
 fn parse(input: &str) -> Vec<Pair> {
     let lines = input.lines()
         .map(|l| l.split_whitespace()
             .skip(1)
-            .map(|n| n.parse::<u32>().unwrap()).collect::<Vec<u32>>()
+            .map(|n| n.parse::<u64>().unwrap()).collect::<Vec<u64>>()
         )
-        .collect::<Vec<Vec<u32>>>();
+        .collect::<Vec<Vec<u64>>>();
 
     (0..lines[0].len()).into_iter()
         .map(|i| Pair{time: lines[0][i], distance: lines[1][i]})
         .collect()
 }
+fn parse2(input: &str) -> Pair {
+    let lines = input.lines()
+        .map(|l| l.split_whitespace()
+            .skip(1)
+            .fold("".to_string(), |s1, s2| s1+s2)
+        )
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
+
+    Pair{time: lines[0], distance: lines[1]}
+}
 
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let input = parse(input);
     let ranges = input.iter()
         .map(|race| solve_equation(race.time, race.distance));
-    let count = ranges.map(|r| r.len() as u32).product();
+    let count = ranges.map(|r| r.end - r.start).product();
     Some(count)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let input = parse2(input);
+    let range = solve_equation(input.time, input.distance);
+    Some(range.end - range.start)
 }
 
 advent_of_code::main!(6);
@@ -69,8 +82,10 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", 6));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71503));
     }
 }
 
 // 800280
+
+// 45128024
