6246c425607a1ec90947374b83bc7b31d4cb8f22
fgdou <fabigoardou@gmail.com>
2023-12-17 16:54:22 +0100

    maybe done

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,10 +1,10 @@
-use std::collections::HashSet;
+use std::{collections::{HashSet, HashMap}, cmp::Reverse};
 
 use cgmath::Vector2;
 
 type Input = Vec<Vec<u32>>;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Hash, Eq)]
 enum Direction {
     Up, Down, Left, Right
 }
@@ -24,42 +24,117 @@ fn parse(input: &str) -> Input {
     }).collect()
 }
 
-fn visit(input: &Input, pos: Vector2<i32>, straight_count: u32, dir: &Direction, mut visited: HashSet<Vector2<i32>>) -> Option<u32> {
-    if pos.x == input[0].len() as i32-1 && pos.y == input.len() as i32-1 {
-        return Some(0);
-    }
-    if pos.x < 0 || pos.x >= input[0].len() as i32 || pos.y < 0 || pos.y >= input.len() as i32 {
-        return None;
-    }
-    if visited.contains(&pos) {
-        return None;
-    }
-    visited.insert(pos.clone());
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct Node {
+    pos: Vector2<i32>,
+    from: Option<Vector2<i32>>,
+    value: u32,
+    dir: Option<Direction>,
+    straight: u32
+}
 
-    let mut moves = vec!();
+fn print(input: &Input, map: &HashSet<Vector2<i32>>) {
+    let s: String = (0..input.len()).into_iter().map(|y| {
+        (0..input[0].len()).into_iter().map(|x| {
+            if let Some(v) = map.get(&Vector2::new(x as i32, y as i32)) {
+                '*'
+            } else {
+                '.'
+            }
+        }).collect::<String>()
+    }).collect::<Vec<String>>().join("\n");
 
-    if straight_count < 3 {
-        moves.push(visit(input, move_vec(&pos, dir), straight_count+1, dir, visited.clone()));
+    println!("{}", s);
+}
+
+fn visit2(input: &Input) -> HashMap<Vector2<i32>, Node> {
+    let mut visited: HashMap<Vector2<i32>, Node> = HashMap::new();
+    let mut stack: Vec<Node> = Vec::new();
+
+    stack.push(Node{
+        dir: Some(Direction::Up),
+        from: None,
+        pos: Vector2::new(0, 0),
+        straight: 0,
+        value: 0
+    });
+
+    while !stack.is_empty() {
+        stack.sort_by_key(|e| Reverse(e.value));
+
+
+        let value = stack.pop().unwrap();
+
+        for d in vec!(Direction::Left, Direction::Right, Direction::Up, Direction::Down) {
+            let new_pos = move_vec(&value.pos, &d);
+
+            if new_pos.x < 0 || new_pos.y < 0 || new_pos.x >= input[0].len() as i32 || new_pos.y >= input.len() as i32 {
+                continue;
+            }
+
+            if visited.contains_key(&new_pos) {
+                continue;
+            }
+
+            let straight = match &value.dir {
+                Some(d2) if &d == d2 => value.straight + 1,
+                _ => 0
+            };
+
+            if straight > 3 {
+                continue;
+            }
+
+            let node = Node {
+                from: Some(value.pos),
+                dir: Some(d),
+                pos: new_pos,
+                straight,
+                value: value.value + input[new_pos.y as usize][new_pos.x as usize]
+            };
+
+            if let Some(v2) = stack.iter().find(|e| e.pos == new_pos) {
+                if v2.value < node.value {
+                    continue;
+                }
+            }
+
+            stack.push(node);
+        }
+
+        visited.insert(value.pos.clone(), value);
     }
 
-    match dir {
-        Direction::Up | Direction::Down => {
-            moves.push(visit(input, move_vec(&pos, &Direction::Left), 0, &Direction::Left, visited.clone()));
-            moves.push(visit(input, move_vec(&pos, &Direction::Right), 0, &Direction::Right, visited.clone()));
-        },
-        Direction::Left | Direction::Right => {
-            moves.push(visit(input, move_vec(&pos, &Direction::Up), 0, &Direction::Up, visited.clone()));
-            moves.push(visit(input, move_vec(&pos, &Direction::Down), 0, &Direction::Down, visited.clone()));
-        },
+    visited
+}
+
+fn get_path(map: &HashMap<Vector2<i32>, Node>, input: &Input) -> HashSet<Vector2<i32>> {
+    let mut current = Vector2::new(input[0].len() as i32-1, input.len() as i32-1);
+    let mut vecs = HashSet::new();
+
+    while current.x != 0 || current.y != 0 {
+        vecs.insert(current.clone());
+        current = map.get(&current).unwrap().from.unwrap();
     }
 
-    Some(moves.into_iter().filter_map(|e| e).min()? + 1)
+    vecs
+}
+
+fn count(map: &HashSet<Vector2<i32>>, input: &Input) -> u32 {
+    map.iter().map(|p| input[p.y as usize][p.x as usize]).sum()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let input = parse(input);
 
-    visit(&input, Vector2::new(0, 0), 0, &Direction::Right, HashSet::new())
+    let map = visit2(&input);
+    let path = get_path(&map, &input);
+
+    print(&input, &path);
+
+    let cnt = count(&path, &input);
+
+    Some(cnt)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
